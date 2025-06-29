5effba03c3e6ef44444e03abd164516cf58a2209
MrForestino <devivo.dev@gmail.com>
2025-06-30 01:47:58 +0200

    Update supabase.client.js

--- src/services/supabase.client.js
+++ src/services/supabase.client.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 
-export const supabaseClient = createClient(
-	import.meta.env.VITE_SUPABASE_URL,
-	import.meta.env.VITE_SUPABASE_KEY
-)
+// export const supabaseClient = createClient(
+// 	import.meta.env.VITE_SUPABASE_URL,
+// 	import.meta.env.VITE_SUPABASE_KEY
+// )
