4abaded24b2f196c630bf0ca7ea4fcfbe54d1546
MrForestino <devivo.dev@gmail.com>
2025-06-30 14:24:24 +0200

    update

--- src/services/player.service.js
+++ src/services/player.service.js
@@ -1,33 +1,33 @@
-// import { supabaseClient } from './supabase.client'
+import { supabaseClient } from './supabase.client'
 
-// class Player {
-// 	getCurrentPlayer() {
-// 		if (typeof window !== 'undefined') {
-// 			const user = window.Telegram?.WebApp?.initDataUnsafe?.user
+class Player {
+	getCurrentPlayer() {
+		if (typeof window !== 'undefined') {
+			const user = window.Telegram?.WebApp?.initDataUnsafe?.user
 
-// 			if (user && user.id) {
-// 				return {
-// 					tg_id: user.id.toString(),
-// 					username:
-// 						user.username?.trim() || user.first_name?.trim() || 'Anonymous'
-// 				}
-// 			}
-// 		}
+			if (user && user.id) {
+				return {
+					tg_id: user.id.toString(),
+					username:
+						user.username?.trim() || user.first_name?.trim() || 'Anonymous'
+				}
+			}
+		}
 
-// 		return {
-// 			tg_id: 'guest-' + Date.now(),
-// 			username: 'Anonymous'
-// 		}
-// 	}
+		return {
+			tg_id: 'guest-' + Date.now(),
+			username: 'Anonymous'
+		}
+	}
 
-// 	async createPlayer() {
-// 		const { tg_id, username } = this.getCurrentPlayer()
-// 		const { error } = await supabaseClient
-// 			.from('players')
-// 			.insert({ tg_id: tg_id, username: username })
+	async createPlayer() {
+		const { tg_id, username } = this.getCurrentPlayer()
+		const { error } = await supabaseClient
+			.from('players')
+			.insert({ tg_id: tg_id, username: username })
 
-// 		return { error }
-// 	}
-// }
+		return { error }
+	}
+}
 
-// export const PlayerService = new Player()
+export const PlayerService = new Player()
